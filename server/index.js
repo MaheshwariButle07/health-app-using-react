55f5049ab28135f1dd8bfdf7e01fd1c4a8e9dae0
Aditi-Wanjari06 <aditiWan2003@gmail.com>
2024-07-31 20:55:25 +0530

    Initial setup is ready

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
 
-function index() {
-  return (
-    <div>index</div>
-  )
-}
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-export default index
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGODB_URL)
+
+  if (conn) {
+    console.log("MongoDB connected succesfully✅")
+  }
+};
+connectDB();
+
+app.get("/", (req, res) => {
+  res.json({
+      message: "Welcome to Flexfit API"
+  })
+})
+
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
