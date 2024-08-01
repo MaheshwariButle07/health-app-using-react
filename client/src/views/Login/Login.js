c776f095147c2e3be0c74836132bc5c1f6d8c03b
MaheshwariButle07 <maheshwaributle07@gmail.com>
2024-08-01 23:47:34 +0530

    updated login page

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,38 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 
 function Login() {
   return (
     <div>
-    <h1 className='auth-heading'>Before you start please Login first 🡆</h1>
+      <h1 className="auth-heading">Before you start please Login first 🡆</h1>
 
-    <form className='auth-form'>
-        
-
+      <form className="auth-form">
+        <label htmlFor="email" className="auth-form-heading">
+          Enter email:
+        </label>
         <input
-            type='email'
-            placeholder='Enter Email'
-            className='user-input'
+          type="email"
+          id="email"
+          placeholder="email"
+          className="user-input"
+          // value={user.email}
+          // onChange={(e) => {
+          //     setUser({ ...user, email: e.target.value })
+          // }}
         />
 
+        <label htmlFor="password" className="auth-form-heading">
+          Enter password:
+        </label>
         <input
-            type='password'
-            placeholder='Enter Password'
-            className='user-input'
+          type="password"
+          id="password"
+          placeholder="password"
+          className="user-input"
+          // value={user.password}
+          // onChange={(e) => {
+          //     setUser({ ...user, password: e.target.value })
+          // }}
         />
 
         <button
-            type='button'
-            className='auth-btn'>
-            Login
+          type="button"
+          className="auth-btn"
+          //
+        >
+          Login
         </button>
 
-        <Link>
-        <p>If You Don't Have Account ? Signup . </p>
+        <Link to="/login" >
+          <p>Don't have an account?Signup</p>
         </Link>
-    </form>
-</div>
-  )
+      </form>
+
+      <Toaster />
+    </div>
+  );
 }
 
-export default Login
+export default Login;
